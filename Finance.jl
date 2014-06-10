81a43c53e141197706693843b2bc8ff09e26b18f
arnaudamiel <aamiel@gmail.com>
2014-06-10 09:06:13 +0100

    Update Finance.jl

--- Finance.jl
+++ Finance.jl
@@ -1,7 +1,11 @@
 function FV(Rate=0,Nper=0,Pmt=0,Pv=0,Type=0)
-    round(Pv*(1+Rate)^Nper,2)
+    round((-1*Pv*(1 + Rate)^Nper - Pmt*(1 + Rate*Type)*(((1 + Rate)^Nper - 1)/Rate)),2)
 end
 
 function PV(Rate=0,Nper=0,Pmt=0,Fv=0,Type=0)
-    round(Fv/(1+Rate)^Nper,2)
+    round((-1*Fv - Pmt*(1 + Rate*Type)*(((1 + Rate)^Nper - 1)/Rate))/(1 + Rate)^Nper,2)
+end
+
+function PMT(Rate=0,Nper=0,Pv=0,Fv=0,Type=0)
+    round((-1*Fv - Pv*(1 + Rate)^Nper)/((1 + Rate*Type)*((1 + Rate)^Nper - 1)/Rate),2)
 end
